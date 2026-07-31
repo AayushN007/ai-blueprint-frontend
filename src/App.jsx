d59016f6ee174729cd0cc015dd4d3c5422ee2a01
AayushN007 <hemanat2004@gmail.com>
2026-07-31 20:05:55 +0530

    Connect React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import Landing from "./pages/Landing";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 function App() {
-  return <Landing />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
